2c18a34c835fdfa9c4bc2f92d8b74719753d79b3
Jordan Higgins <jordanhiggins@usf.edu>
2025-02-22 15:14:22 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -40,3 +40,27 @@ console.log(mgr1.getDetails());
 
 console.log(mgr1.calculateBonus()); 
 // Expected output: 9600
+
+// Task 3: Created Company Class
+class Company {
+    constructor(name) {
+        this.name = name;
+        this.employees = [];
+    };
+
+    addEmployee(employee) {
+        this.employees.push(employee);
+    };
+
+    listEmployees() {
+        this.employees.forEach(employee => console.log(employee.getDetails()));
+    };
+};
+
+const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+// Expected output:
+// "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+// "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
